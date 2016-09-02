db854266b312b2eaa654668eb90888f89edffa9a
Praveena <praveena.manikonda@gmail.com>
2016-09-02 00:31:30 +0000

    Add full title helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  # Returns a full title on a pe- page basis 
+  def full_title(page_title = '')
+      base_title = "Ruby On Rails Application"
+      if page_title.empty? 
+          base_title
+      else 
+          page_title + " | " + base_title
+      end 
+  end 
 end
